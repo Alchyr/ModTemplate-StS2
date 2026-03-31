45eca0966934fcb891878d409a2acf580869dcba
Alchyr <isithalchyr@gmail.com>
2026-03-31 14:08:32 -0700

    Remove placeholder as MainFile is now here

--- content/ModTemplate/ModTemplateCode/Placeholder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ModTemplate.ModTemplateCode;
-
-public class Placeholder
-{
-    //You're recommended but not required to keep all your code in this package and all your assets in the ModTemplate folder.
-}
