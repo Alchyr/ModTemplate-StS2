f9e382e55168cba4849a22c121cfc21ec148c026
zBinFinn <84500718+zBinFinn@users.noreply.github.com>
2026-04-15 23:24:53 +0200

    feat: add fallback for card image paths

--- content/CharacterModTemplate/CharModCode/Extensions/StringExtensions.cs
+++ content/CharacterModTemplate/CharModCode/Extensions/StringExtensions.cs
@@ -10,11 +10,22 @@ public static class StringExtensions
     
     public static string CardImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "card_portraits", path);
+        var cardPath = Path.Join(MainFile.ResPath, "images", "card_portraits", path);
+        if (!File.Exists(cardPath))
+        {
+            cardPath = Path.Join(MainFile.ResPath, "images", "card_portraits", "card.png");
+        }
+        return cardPath;
     }
+    
     public static string BigCardImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "card_portraits", "big", path);
+        var cardPath = Path.Join(MainFile.ResPath, "images", "card_portraits", "big", path);
+        if (!File.Exists(cardPath))
+        {
+            cardPath = Path.Join(MainFile.ResPath, "images", "card_portraits", "big", "card.png");
+        }
+        return cardPath;
     }
 
     public static string PowerImagePath(this string path)
